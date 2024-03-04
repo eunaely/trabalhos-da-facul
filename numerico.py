8b3dfe74fb01e863d7747bbf1670dbbb5bb8b64e
Cássia Naelir de Lima <116851542+eunaely@users.noreply.github.com>
2024-03-04 19:19:32 -0300

    Add files via upload

---
+++ numerico.py
@@ -0,0 +1,8 @@
+# Solicita a entrada do usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se o número é par ou ímpar
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
