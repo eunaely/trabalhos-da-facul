6710f540f5751caf5c6676d83800b687b8857782
Cássia Naelir de Lima <116851542+eunaely@users.noreply.github.com>
2024-03-14 19:20:06 -0300

    Add files via upload
    foi solicitado um programa que solicita ao usuário a digitação de números. O
    programa só deve parar de solicitar digitação quando o usuário digitar
    "FIM". Após isso, os números digitados devem ser armazenados em uma
    lista. Por fim, o programa deverá criar uma nova lista, obrigatoriamente
    usando "list comprehension", com os elementos tendo seus valores sendo
    o triplo dos valores da primeira lista.

---
+++ # Solicita a entrada do usuário para a t.py
@@ -0,0 +1,8 @@
+# Solicita a entrada do usuário para a temperatura em Celsius
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+# Converte Celsius para Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Exibe o resultado
+print(f"A temperatura em Fahrenheit é: {fahrenheit}")
